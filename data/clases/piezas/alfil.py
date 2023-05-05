187762195a356c93b7160908117641959ffdc86e
lucasburgosr <lucaasburgos13@gmail.com>
2023-05-05 12:52:05 -0300

    Clase alfil terminada

--- data/clases/piezas/alfil.py
+++ data/clases/piezas/alfil.py
@@ -8,3 +8,44 @@ class Alfil(Pieza):
         self.img = pygame.image.load(img_path)
         self.img = pygame.transform.scale(self.img, (tablero.tile_ancho - 20, tablero.tile_alto - 20))
         self.notation = 'A'
+
+    def get_movimientos_validos(self, tablero):
+        salida = []
+
+        mueve_ne = []
+        for i in range(1, 8):
+            if self.x + i > 7 or self.y - i < 0:
+                break
+            mueve_ne.append(tablero.get_cuadricula_desde_pos(
+                (self.x + i, self.y - i)
+            ))
+        salida.append(mueve_ne)
+
+        mueve_se = []
+        for i in range(1, 8):
+            if self.x + i > 7 or self.y + i > 7:
+                break
+            mueve_se.append(tablero.get_cuadricula_desde_pos(
+                (self.x + i, self.y + i)
+            ))
+        salida.append(mueve_se)
+
+        mueve_so = []
+        for i in range(1, 8):
+            if self.x - i < 0 or self.y + i > 7:
+                break
+            mueve_so.append(tablero.get_cuadricula_desde_pos(
+                (self.x - i, self.y + i)
+            ))
+        salida.append(mueve_so)
+
+        mueve_no = []
+        for i in range(1, 8):
+            if self.x - i < 0 or self.y - i < 0:
+                break
+            mueve_no.append(tablero.get_cuadricula_desde_pos(
+                (self.x - i, self.y - i)
+            ))
+        salida.append(mueve_no)
+        
+        return salida
