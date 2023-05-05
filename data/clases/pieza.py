b87626d901719f575b93c04c2a5ef279ef39f7ab
juancruzfilippini <juan.filippini@alumnos.frm.utn.edu.ar>
2023-05-05 11:36:12 -0300

    definiendo funciones get_movimiento y get_movimientos_validos

--- data/clases/pieza.py
+++ data/clases/pieza.py
@@ -7,3 +7,24 @@ class Pieza:
         self.y = pos[1]
         self.color = color
         self.se_ha_movido = False
+
+    def get_movimientos(self, tablero):
+        output = []
+        for direction in self.get_posibles_movimientos(tablero):
+            for cuadricula in direction:
+                if cuadricula.ocupando_espacio is not None:
+                    if cuadricula.ocupando_espacio.color == self.color:
+                        break
+                    else:
+                        output.append(cuadricula)
+                        break
+                else:
+                    output.append(cuadricula)
+        return output
+    
+    def get_movimientos_validos(self, tablero):
+        output = []
+        for cuadricula in self.get_movimientos(tablero):
+            if not tablero.esta_en_jaque(self.color, cambio_del_tablero=[self.pos, cuadricula.pos]):
+                output.append(cuadricula)
+        return output
