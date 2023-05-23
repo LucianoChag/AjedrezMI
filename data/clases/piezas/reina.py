5e235392e73a73009e9e6c5f66202e6ff0dfd5bd
Luciano Chagnaud <lucianochagnaud@gmail.com>
2023-05-23 18:09:11 -0300

    generacion codigo torre.py

--- data/clases/piezas/reina.py
+++ data/clases/piezas/reina.py
@@ -4,7 +4,7 @@ from data.clases.pieza import Pieza
 class Reina(Pieza):
     def __init__(self, pos, color, tablero):
         super().__init__(pos, color, tablero)
-        img_path = 'data/imgs/' + color[0] + '_queen.png'
+        img_path = 'data/imgs/' + color[0] + '_reina.png'
         self.img = pygame.image.load(img_path)
         self.img = pygame.transform.scale(self.img, (tablero.tile_ancho - 20, tablero.tile_alto - 20))
         self.notation = 'Q'
